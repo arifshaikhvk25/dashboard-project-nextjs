6455b1b3b97bbfbea1b25adbee12ae1a2fd15ad0
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2023-01-25 02:03:50 +0530

    label:sales

--- Components/BarChart.jsx
+++ Components/BarChart.jsx
@@ -36,7 +36,7 @@ const BarChart = () => {
 
             datasets: [
                 {
-                    labels: "Sales $",
+                    label: "Sales $",
                     data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
                     borderColor: 'rgb(53, 162, 235)',
                     backgroundColor: 'rgb(53, 162, 235, 0.4'
